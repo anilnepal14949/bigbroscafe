9132123563d523f63706b41faea9db4136e0354f
Anil Nepal <anilnepal14949@gmail.com>
2020-08-07 17:06:24 +1000

    Added components used in frontend

--- resources/js/app.js
+++ resources/js/app.js
@@ -27,6 +27,22 @@ export default router;
 Vue.component("admin", require("./components/Admin.vue").default);
 Vue.component("sidebar", require("./components/Sidebar.vue").default);
 Vue.component("navbar", require("./components/Navbar.vue").default);
+Vue.component(
+    "main-navbar",
+    require("./components/Frontend/Navbar.vue").default
+);
+Vue.component(
+    "main-slider",
+    require("./components/Frontend/Slider.vue").default
+);
+Vue.component(
+    "special-menu",
+    require("./components/Frontend/SpecialMenu.vue").default
+);
+Vue.component(
+    "frontend",
+    require("./components/Frontend/Frontend.vue").default
+);
 
 const app = new Vue({
     el: "#app",
