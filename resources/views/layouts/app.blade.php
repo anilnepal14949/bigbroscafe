9f042b12d8a283cb1682bc07a4b99b279ccf5f99
Anil Nepal <anilnepal14949@gmail.com>
2020-08-07 17:08:10 +1000

    Updated layout and home files to adapt to frontend

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,96 +8,41 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Ecommerce') }}</title>
+    <title>Big Bro's Cafe & Restro</title>
+    <link href="{{ asset('logoico.png') }}" rel="icon">
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <style href="{{ asset('css/stripe.css') }}"></style>
+    <!-- <style href="{{ asset('css/stripe.css') }}"></style> -->
+    <style>
+        * {
+            box-sizing: border-box;
+            margin: 0;
+            padding: 0;
+        }
+
+        body {
+            background-color: #24252a;
+        }
+    </style>
     @notifyCss
 </head>
 
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Ecommerce') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        @if(Auth::check())
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('orders') }}">
-                                Orders
-                            </a>
-                        </li>
-                        @endif
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('cart.show') }}"><span class="fas fa-shopping-cart">
-                                    {{session()->has('cart')?session()->get('cart')->totalQty:0}}
-                                </span></a>
-                        </li>
-                        <!-- Authentication Links -->
-                        @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                        @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                        @endif
-                        @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                @if(auth()->user()->is_admin == 1)
-                                <a class="dropdown-item" href="{{ route('dashboard') }}">
-                                    Admin Dashboard
-                                </a>
-                                @endif
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
+        <main-navbar></main-navbar>
+        @yield('content')
     </div>
     <!-- Scripts -->
     <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
 
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="https://js.stripe.com/v3/"></script>
-    <script src="{{ asset('js/stripe.js') }}"></script>
+    <script src="{{ asset('js/script.js') }}"></script>
+    <!-- <script src="https://js.stripe.com/v3/"></script>
+    <script src="{{ asset('js/stripe.js') }}"></script> -->
 
     @include('notify::messages')
     @notifyJs
